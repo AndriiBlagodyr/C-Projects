59b7aae3869e9b42bfa716e27b29e1f0e80d0320
Mr_Anderson <andrii.blagodyr@gmail.com>
2020-02-09 13:57:33 +0300

    proj(CMT-8)Replacing chars in the math expressions

---
+++ ReplaceCharInMathExpression/ReplaceCharInMathExpression/Methods.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReChangeMathOperators
+{
+    public class Methods
+    {
+        public static List<string> Combinations(int val)
+        {
+            string[] operators = { "+", "-", "*", "/" };
+
+            int[] array = new int[val];
+
+            int capacity = (int)Math.Pow(operators.Length, array.Length);
+
+            List<string> collection = new List<string>(capacity);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 0;
+            }
+
+            while (true)
+            {
+                string y = null;
+                for (int z = 0; z < array.Length; z++)
+                {
+                    y += operators[array[z]];  // replace 0,1,2... for  '+','-','*'...
+                }
+                collection.Add(y); // Add collection element
+
+                int i = array.Length - 1;
+
+                for (; i >= 0; i--)
+                {
+                    array[i] += 1;
+
+                    if (array[i] == operators.Length)
+                    {
+                        array[i] = 0;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (i < 0)
+                {
+                    break;
+                }
+            }
+
+            return collection;
+        }
+
+        public ArrayList OPNReverseString(string input)
+        {
+            Regex patern = new Regex(@"\(\-\d+\)|\(|\)|\+|\-|\*|\/|(\d+\.?\d*)|()");
+            Regex numbers = new Regex(@"\d+\.?\d*"); // numbers
+            Regex minnumbers = new Regex(@"\(\-\d+\)"); // - min numbers
+            Regex brackets = new Regex(@"\(|\)"); // brackets
+            Regex operators = new Regex(@"\(|\)|\+|\-|\*|\/"); // operators     
+            Stack stackOpers = new Stack();
+            ArrayList expression = new ArrayList();
+
+            MatchCollection mathexpr = patern.Matches(input);
+
+            foreach (Match value in mathexpr)
+            {
+                Match val;
+                val = minnumbers.Match(value.Value);
+                if (val.Success)
+                {
+                    string s = String.Empty;
+                    s = val.Value.Substring(1, val.Value.Length - 2);
+                    expression.Add(s); continue;
+                }
+                val = numbers.Match(value.Value);
+                if (val.Success) { expression.Add(val.Value); continue; }
+                val = brackets.Match(value.Value);
+                if (val.Success)
+                {
+                    if (val.Value == "(") { stackOpers.Push(val.Value); continue; }
+                    string op = stackOpers.Pop().ToString();
+                    while (op != "(")
+                    {
+                        expression.Add(op);
+                        op = stackOpers.Pop().ToString();
+                    }
+                    continue;
+                }
+                val = operators.Match(value.Value);
+                if (val.Success)
+                {
+                    try
+                    {
+                        while (GetPriority(val.Value) <= GetPriority((string)stackOpers.Peek()))
+                        {
+                            if (stackOpers.Peek().ToString() == "(") break;
+                            expression.Add(stackOpers.Pop().ToString());
+                        }
+                    }
+                    catch (System.Exception)
+                    {
+                        // Empty stack exception
+                    }
+                    stackOpers.Push(val.Value);
+                }
+            }
+            while (stackOpers.Count != 0)
+            {
+                expression.Add(stackOpers.Pop().ToString());
+            }
+            return expression;
+        }
+
+        //Method of operator's priority
+        public static byte GetPriority(string s)
+        {
+            switch (s)
+            {
+                case "(": return 0;
+                case ")": return 0;
+                case "+": return 1;
+                case "-": return 1;
+                case "*": return 2;
+                case "/": return 2;
+                default: return 7;
+            }
+        }
+
+        // Counting result from OPN method
+        public double Counting(ArrayList expression)
+        {
+            double result = 0; //Result
+            Stack<double> temp = new Stack<double>(); //Temporary stack
+
+            string[] array = new string[expression.Count];
+
+            for (int i = 0; i < expression.Count; i++)
+            {
+                array[i] = (string)expression[i];
+            }
+
+            Regex numbers = new Regex(@"^\d+$"); // numbers
+            Regex numbers1 = new Regex(@"^-[0-9]*[1-9][0-9]*$"); // min numbers
+            Regex operators = new Regex(@"\(|\)|\+|\-|\*|\/"); // operators
+
+            for (int i = 0; i < expression.Count; i++)
+            {
+                if (numbers1.IsMatch(array[i]) | numbers.IsMatch(array[i])) // Check if value is equal to operands
+                {
+                    temp.Push(double.Parse(array[i]));
+                }
+                else if (operators.IsMatch(array[i])) // Check if value is equal to operators
+                {
+                    try
+                    {
+                        double a = temp.Pop();
+                        double b = temp.Pop();
+                        if (a == 0 & array[i] == "/")
+                        {
+                            try
+                            {
+                                //Console.WriteLine("Devide by zero exseption is detected");
+                                throw new DivideByZeroException();
+                            }
+                            catch (DivideByZeroException)
+                            {
+
+                                return -1.157;  // DivideByZeroException
+                            }
+                        }
+                        switch (array[i])
+                        {
+                            case "+": result = b + a; break;
+                            case "-": result = b - a; break;
+                            case "*": result = b * a; break;
+                            case "/": result = b / a; break;
+                        }
+                    }
+                    catch
+                    {
+                        // Empty stack exception
+                    }
+                    temp.Push(result);
+                }
+            }
+            return temp.Peek();
+        }
+    }
+
+}
---
+++ ReplaceCharInMathExpression/ReplaceCharInMathExpression/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+
+
+// Replace char '?' in math expresion "20?30?40?10=10?(20?10)" that the left expression part was equal to the right part of expression
+namespace ReChangeMathOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Insert math expression:");
+
+                string input = Console.ReadLine();
+            //string input = "20?((30?20)?10)=20?20";
+           // string input = "20?30?40?10=10?(20?10)";
+            // string input = "10?10=40?10?10";
+
+            string[] operators = { "+", "-", "*", "/" };
+
+            input = input.Replace(" ", string.Empty);  // string without space chars
+
+            string left = input.Remove(input.IndexOf("="));
+            string right = input.Substring(1 + input.IndexOf("="));
+
+            string[] leftArr = left.Split('?');  // Array consists of elements separeted with char '?'
+            string[] rightArr = right.Split('?');
+
+            int leftCounter, rightCounter, leftCapacity, rightCapacity = 0;
+
+            leftCounter = leftArr.Length - 1;       //3 chars '?'    leftArr.Length - 1
+            rightCounter = rightArr.Length - 1;     //2 chars '?'    rightArr.Length - 1
+
+            leftCapacity = (int)Math.Pow(operators.Length, leftCounter);  // Quantity of possible combinations. Collection Capacity. Pow(4,3)
+            rightCapacity = (int)Math.Pow(operators.Length, rightCounter); // Pow(4,2)
+
+
+            List<string> leftCol = Methods.Combinations(leftCounter);  // Collection of operators' combinations for one part of the string
+            List<string> rightCol = Methods.Combinations(rightCounter);
+
+            NameValueCollection leftname = new NameValueCollection(leftCapacity);  // Collection Key-Value with all available results
+            NameValueCollection rightname = new NameValueCollection(rightCapacity);
+
+            for (int i = 0; i < leftCapacity; i++)  // Add elements to leftname Collection
+            {
+                StringBuilder builder = new StringBuilder();
+
+                for (int j = 0; j < leftCounter; j++)
+                {
+                    builder.Append(leftArr[j]).Append(((string)leftCol[i])[j]);
+                }
+                builder.Append(leftArr[leftCounter]);
+
+                string build1 = builder.ToString();
+
+                Methods instance = new Methods();
+
+                string value = instance.Counting(instance.OPNReverseString(build1)).ToString();
+
+                leftname.Add(build1, value);
+            }
+
+            for (int i = 0; i < rightCapacity; i++)  // Add elements to rightname Collection
+            {
+                StringBuilder builder = new StringBuilder();
+
+                for (int j = 0; j < rightCounter; j++)
+                {
+                    builder.Append(rightArr[j]).Append(((string)rightCol[i])[j]);
+                }
+                builder.Append(rightArr[rightCounter]);
+
+                string build1 = builder.ToString();
+
+                Methods instance = new Methods();
+
+                string value = instance.Counting(instance.OPNReverseString(build1)).ToString();
+
+                rightname.Add(build1, value);
+            }
+            bool flag = false;
+            for (int i = 0; i < leftname.Count; i++)
+            {
+                for (int j = 0; j < rightname.Count; j++)
+                {
+                    if (leftname.Get(i) == rightname.Get(j))
+                    {
+                        Console.WriteLine(leftname.GetKey(i) + " = " + rightname.GetKey(j) + "     result=" + rightname.Get(j));
+
+                        flag = true;
+                    }
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("No matches is found!");
+            }
+          }
+
+            
+        }
+    }
+}
+
